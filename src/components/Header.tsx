7818045fb312b6e4764012296c5ca28dbac92e7b
zakaria hamdino <hamdinozakaria7@gmail.com>
2022-07-06 00:46:06 +0100

    still working on header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between w-8/12'>
+        <div>
+            <img src="" alt="" />
+            <p className='text-blue-500'>ZAKARIA HAMDINO</p>
+        </div>
+        <div className='flex justify-between w-2/5'>
+            <a href="" className='font-mono'>HOME</a>
+            <a href="" className='font-mono'>ABOUT</a>
+            <a href="" className='font-mono'>SERVICES</a>
+            <a href="" className='font-mono'>CONTACT</a>
+            <button>DOWNLOAD CV</button>
+        </div>
+    </div>
+  )
+}
+export default Header
---
+++ src/components/Introduction.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Introduction = () => {
+  return (
+    <div>
+      <div>
+        <p>HELLO,MY NAME IS </p>
+        <hr  className='bg-[#AFB7C4]'/>
+        <p>Zakaria Hamdino</p>
+        <p>Web Developper</p>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p>Hamdinozakaria12@gmail.com</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>069731-7731</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Introduction
