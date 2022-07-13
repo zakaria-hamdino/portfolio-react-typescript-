a751fe1aea0d62cc70810a8b1e7b135e9255fe5a
zakaria hamdino <hamdinozakaria7@gmail.com>
2022-07-13 18:30:58 +0100

    color changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>
+    <div className='mt-10'>
         <div>
             <p> What I do</p>
             <div>
@@ -10,17 +10,17 @@ const Services = () => {
             </div>
         </div>
         <div className='flex justify-between mt-5'>
-                <div className='w-2/4'>
+                <div>
                     <img src="" alt="" />
                     <p>UI/UX DESIGN</p>
                     <p>An effective UI/UX is what captures attention and spreads a clear message.I make sure the design is innovative and neat with all of this.</p>
                 </div>
-                <div className='w-2/4'>
+                <div>
                     <img src="" alt="" />
                     <p>Web developement</p>
                     <p>if you are looking for a developer who'll take over the research and development of your website,I am a well-established professional to help you with this</p>
                 </div>
-                <div className='w-2/4'>
+                <div>
                     <img src="" alt="" />
                     <p>APP DEVELOPMENT</p>
                     <p>if you are looking for a user-friendly app that will attract more mobile users,I can help you design and build a platfrom with the latest and trendiest look and feel</p>
